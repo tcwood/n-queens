bbb4cca8b4c320c3159590c14c3f98a886eb8663
tcwood <travis.cwood@gmail.com>
2016-09-21 18:36:23 -0700

    Add single solution algorithm for rooks

--- src/solvers.js
+++ src/solvers.js
@@ -16,15 +16,33 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var matrix = new Board({n: n});
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  for (var i = 0; i < n; i++) {
+    matrix.togglePiece(i, i);
+  }
+
+  return matrix.rows();
+
+
+  // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
   var solutionCount = undefined; //fixme
+  //roundsLeft = n  <-- n total addPiece function calls
+  //iterate through all possible starting points (nested loops for rows / cols)
+    //place a rook at each starting point
+      //Recursively call addPiece(nextMove, roundsLeft) function for each possible next move (n^2 - num of curr pieces)
+        //subtract 1 from roundsLeft 
+        //which creates a new board with prior piece in same place
+        //and a rook on a new spot
+          // check for any conflicts
+            //if there are conflicts, STOP! Don't go any deeper, don't return anything
+            //else, recursively call addPiece function
+              //keep going for (n = size) number of recursive calls
+                //if no conflicts, return solution
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
